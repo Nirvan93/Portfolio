7ec7ce4955a8bd5daf2598efcf4cac1b0661738d
Filip Moeglich <tytanm@gmail.com>
2016-03-30 16:27:48 +0200

    update styles

---
+++ css/style-css.css
@@ -0,0 +1,124 @@
+body 
+{
+	color:#f7f7f7;
+	/*background-color: #101010;*/
+	background-image: url("../img/bg.jpg");
+    background-repeat: repeat;
+}
+
+.ac-hbody 
+{
+    background:url(../img/header.png) no-repeat top center;
+}
+
+.ac-topline 
+{
+	background:url(../img/ac_topline.png) repeat-x top center;
+}
+
+h1, h2, h3, h4, h5, h6 
+{
+	color:#e8e5e5;
+}
+
+h1 a, h2 a, h3 a, h4 a, h5 a 
+{ 
+	color:#fff;
+}
+
+h1 a:hover, h2 a:hover, h3 a:hover, h4 a:hover, h5 a:hover, #respond h3 small a 
+{ 
+	color:#101116;
+}
+
+a 
+{
+	/*color:#393E5F;*/
+	color:#CCC;
+}
+
+.topbar 
+{
+	background:rgba(65, 65, 65, 0.35);
+}
+
+.line 
+{
+	border-top:1px solid #e4e4e4;
+}
+
+.thick-line 
+{
+	border-top:6px solid #e4e4e4;
+}
+
+/* ----------------- custom button styles ----------------- */
+.ac-button 
+{
+	background:#ffffff;
+	color:#fff;
+}
+
+/*
+.ac-button:hover 
+{
+	background:rgba(240, 72, 16, 0.1);
+	color:#fff;
+	text-decoration:none;
+}*/
+
+/* ----------------- menu styles ----------------- */
+#menu-wrapper a {
+	color:#fff !important;
+}
+
+/*.sf-menu a:hover 
+{
+	background:rgba(240, 72, 16, 0.1);
+}*/
+
+.sf-menu ul 
+{ /* submenu background */
+	background:#121212;
+}
+
+/* ----------------- footer styles ----------------- */
+#footer 
+{
+	background:rgba(25, 25, 25, 0.6);
+	color:#fff;
+}
+
+#footer a 
+{ 
+	color:#fff;
+}
+
+#footer a:hover 
+{
+	color:#D16727;
+}
+
+#footer h6 
+{ 
+	color:#D16727;
+}
+
+#footer-body 
+{
+	border-top:1px solid #333333;
+	border-bottom:1px solid #2a2a2a;
+	background:#1a1a1a url(images/footer_bg.gif) repeat top left;
+}
+
+#footer ::selection, .topbar ::selection, .spotlight-block ::selection, #cycle-slider ::selection, #zacc-slide div.zacc-info ::selection 
+{ 
+	background:#D16s727;
+	color:#fff; 
+}
+
+#footer ::-moz-selection, .topbar ::-moz-selection, .spotlight-block ::-moz-selection, #cycle-slider ::-moz-selection, #zacc-slide div.zacc-info ::-moz-selection 
+{ 
+	background:#D16727;
+	color:#fff;
+}
