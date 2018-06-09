0782d5d2c0fadb881918caeddf16685118d3a1ca
Fylyp Leno <tytanm@gmail.com>
2018-06-09 12:49:15 +0200

    Update portfolio to 2018

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -14,7 +14,7 @@ var galleryAutoplayTimeout;
 
 $("#front").click(function(e)
 {
-	if( galleryIndex < 14 ) galleryIndex++; else galleryIndex = 0;
+	if( galleryIndex < 15 ) galleryIndex++; else galleryIndex = 0;
 	GalleryTransition();
 });
 
